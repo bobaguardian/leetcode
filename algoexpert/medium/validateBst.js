e8eab37fe1b31d64cf610aaef29993a2ca60db3b
vivian <vivianlthach@gmail.com>
2022-03-23 16:56:43 -0700

    Add comment explaning solution 1 to validateBst.js

--- algoexpert/medium/validateBst.js
+++ algoexpert/medium/validateBst.js
@@ -8,6 +8,10 @@ class BST {
 }
 
 // Solution 1 - Recursion using min and max helper parameters
+// Use min and max to keep track of the range the current node should be in
+// if the current node's value is not within the range, return false
+// recursively call the function with the current's left and right node
+// passing in their respective min and max ranges
 // Time Complexity - O(n), where n = number of nodes (visiting each node)
 // Space Complexity O(d) where d = depth of tree (recursion, call stack)
 function validateBst(tree, min = -Infinity, max = Infinity) {

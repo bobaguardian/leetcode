f792e2ebf8bb2a07eabdf25a986c8b490f895b05
vivian <vivianlthach@gmail.com>
2022-03-24 13:10:26 -0700

    Solve binarySearch with optimal solution: O(log(n)) T, O(1) S - with while loop

--- algoexpert/easy/binarySearch.js
+++ algoexpert/easy/binarySearch.js
@@ -22,3 +22,24 @@ function binarySearchHelper(array, target, low, high) {
 		return binarySearchHelper(array, target, low, midpoint - 1);
 	}
 }
+
+
+// Solution 2 - use low and high to keep track of array ranges, use midpoint to
+// compare with target and search the respective half at each step
+// Time Complexity - O(log(n)) - looks at half the input at each step
+// Space Complexity - O(1) - constant space
+function binarySearch(array, target) {
+	let low = 0;
+	let high = array.length - 1;
+
+	while (low <= high) {
+  	let midpoint = Math.floor((low + high) /2);
+
+		if (array[midpoint] === target) return midpoint;
+
+		if (target < array[midpoint]) high = midpoint - 1;
+		else low = midpoint + 1;
+	}
+
+	return -1;
+}

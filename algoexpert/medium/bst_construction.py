78211a610eaef98954d89e6525a9ac5c0a2c17d4
vivian <vivianlthach@gmail.com>
2022-03-25 02:01:58 -0700

    bst construction in python

---
+++ algoexpert/medium/bst_construction.py
@@ -0,0 +1,17 @@
+class BST:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+
+  def insert(self, value):
+    pass
+
+
+  def contains(self, value):
+    pass
+
+
+  def remove(self, value, parent = None):
+    pass

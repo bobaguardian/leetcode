1c7d9c765d7075923494b624e4256094a9561775
Vivian Thach <vivianlthach@gmail.com>
2022-04-12 16:22:15 -0700

    solve branchSums w/o hints! - O(n) TS, recursive solution - dfs

--- algoexpert/easy/branchSums.js
+++ algoexpert/easy/branchSums.js
@@ -8,6 +8,44 @@ class BinaryTree {
     }
   }
 
+
+  /**
+   *
+   * Solution 1 - recursion - pass in result array and sum at each node
+   * if the node is a leaf node (base case), add sum + val to array else
+   * call recursively on left and right if they're not null
+   *
+   * Time = O(n), n = number of nodes
+   * Space = O(n), n = number of nodes => recursive call stack
+   */
   function branchSums(root) {
     // Write your code here.
+	// go through and sum each branch from left to right and add
+	// the sum to an array
+	// use helper method - recursively, pass in sum at the given point, pass in the result array
+	// base case - if the node is a leaf, push the node.value + sum to the result array
+	// recursive case: call recursive function on the left and on the right
+	// return result array
+	return branchSumsHelper(root, [], 0);
   }
+
+
+  const branchSumsHelper = (node, result = [], sum = 0) => {
+	// base
+	if (!node.left && !node.right) {
+		result.push(node.value + sum);
+	}
+
+	// recursive call on left
+	if (node.left) {
+		branchSumsHelper(node.left, result, sum + node.value)
+	}
+
+	// recursive call on right
+	if (node.right) {
+		branchSumsHelper(node.right, result, sum + node.value)
+	}
+
+	return result;
+
+}

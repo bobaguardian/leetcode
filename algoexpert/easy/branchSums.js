c7585ccdbea379197bd55b77a85c8d18ef3860d7
Vivian Thach <vivianlthach@gmail.com>
2022-04-12 16:08:56 -0700

    branchSums initial commit

---
+++ algoexpert/easy/branchSums.js
@@ -0,0 +1,13 @@
+// This is the class of the input root.
+// Do not edit it.
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  function branchSums(root) {
+    // Write your code here.
+  }

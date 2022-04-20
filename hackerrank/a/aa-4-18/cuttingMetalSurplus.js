03f5e06039972eae1170c773c709e1cc26478fe7
Vivian Thach <vivianlthach@gmail.com>
2022-04-20 15:30:13 -0700

    add initial code for cuttingMetalSurplus

---
+++ hackerrank/a/aa-4-18/cuttingMetalSurplus.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+/*
+ * Complete the 'maxProfit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER costPerCut
+ *  2. INTEGER salePrice
+ *  3. INTEGER_ARRAY lengths
+ *
+ *
+ * need a hash to store profits of each length
+ * key = length, value = profit
+ *
+ * [26, 103, 59]
+ * Honestly I'm stumped on how to start this D:
+ *
+
+*/
+
+
+function maxProfit(costPerCut, salePrice, lengths) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const costPerCut = parseInt(readLine().trim(), 10);
+
+    const salePrice = parseInt(readLine().trim(), 10);
+
+    const lengthsCount = parseInt(readLine().trim(), 10);
+
+    let lengths = [];
+
+    for (let i = 0; i < lengthsCount; i++) {
+        const lengthsItem = parseInt(readLine().trim(), 10);
+        lengths.push(lengthsItem);
+    }
+
+    const result = maxProfit(costPerCut, salePrice, lengths);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
